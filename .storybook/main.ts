3107ddac29d903009983eff22ba397e8dffa8c49
곽성재 <sungjae8970@gmail.com>
2025-04-09 22:51:25 +0900

    fix: storybook dev 안되는현상 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,8 +14,6 @@ const config: StorybookConfig = {
     "name": "@storybook/nextjs",
     "options": {}
   },
-  "staticDirs": [
-    "..\\public"
-  ]
+  // "staticDirs": ["..\\public"], // WARN: 개발서버가 구동이 안되어서 주석처리 FIXME: 
 };
 export default config;
