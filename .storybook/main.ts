6a78ad07eede5d911ee2b60183e0410bade385f3
곽성재 <sungjae8970@gmail.com>
2025-04-22 01:35:01 +0900

    style: 커스텀 스타일이 적용되도록 코드 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,16 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
   },
-  // "staticDirs": ["..\\public"], // WARN: 개발서버가 구동이 안되어서 주석처리 FIXME: 
+  // "staticDirs": ["..\\public"], // WARN: 개발서버가 구동이 안되어서 주석처리 FIXME:
 };
 export default config;
