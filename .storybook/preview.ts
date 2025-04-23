974055411c38fe456c81174bf2b5b28f3a4dfe27
곽성재 <sungjae8970@gmail.com>
2025-04-23 18:15:10 +0900

    INIT: storybook break poin init

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -10,6 +10,45 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports: {
+        xs: {
+          name: "XS (375–767)",
+          styles: {
+            width: "375px",
+            height: "800px",
+          },
+        },
+        sm: {
+          name: "SM (768–991)",
+          styles: {
+            width: "768px",
+            height: "800px",
+          },
+        },
+        md: {
+          name: "MD (992–1199)",
+          styles: {
+            width: "992px",
+            height: "800px",
+          },
+        },
+        lg: {
+          name: "LG (1200–1599)",
+          styles: {
+            width: "1200px",
+            height: "800px",
+          },
+        },
+        xl: {
+          name: "XL (1600+)",
+          styles: {
+            width: "1600px",
+            height: "800px",
+          },
+        },
+      },
+    },
   },
 };
 
