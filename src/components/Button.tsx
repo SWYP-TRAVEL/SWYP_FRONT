c0b6d6902e033a1207f55a567275049cdf8320d4
Ahnyeongjun <toinin@naver.com>
2025-04-30 14:19:44 +0900

    [FIX] button default width,height, card flex direction

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,14 +15,14 @@ type ButtonProps<T extends ElementType = "button"> = {
 } & React.ComponentPropsWithoutRef<T>;
 
 const BUTTON_VARIANT_STYLES: Record<ButtonVariant, string> = {
-  default: "min-w-[268px] h-[56px] rounded-[28px] px-6 py-4 gap-[10px] bg-[#9A77FF] hover:bg-[#7C49FF] active:bg-[#5F23EB] text-white",
-  confirm: "min-w-[268px] h-[56px] rounded-[28px] px-6 py-4 gap-[10px] bg-black text-white",
-  gradation: "min-w-[268px] h-[50px] rounded-[25px] px-6 py-4 gap-2 bg-[linear-gradient(125.9deg,_#9A77FF_23.39%,_#214BFF_104.52%)] text-white",
-  hover: "min-w-[268px] h-[56px] rounded-[28px] px-6 py-4 gap-[10px] bg-[#7C49FF] text-white",
-  press: "min-w-[268px] h-[56px] rounded-[28px] px-6 py-4 gap-[10px] bg-[#5F23EB] text-white",
+  default: "min-w-[90px] h-[44px] rounded-[28px] px-6 py-4 gap-[10px] bg-[#9A77FF] hover:bg-[#7C49FF] active:bg-[#5F23EB] text-white",
+  confirm: "min-w-[90px] h-[44px] rounded-[28px] px-6 py-4 gap-[10px] bg-black text-white",
+  gradation: "min-w-[90px] h-[44px] rounded-[25px] px-6 py-4 gap-2 bg-[linear-gradient(125.9deg,_#9A77FF_23.39%,_#214BFF_104.52%)] text-white",
+  hover: "min-w-[90px] h-[44px] rounded-[28px] px-6 py-4 gap-[10px] bg-[#7C49FF] text-white",
+  press: "min-w-[90px] h-[44px] rounded-[28px] px-6 py-4 gap-[10px] bg-[#5F23EB] text-white",
 };
 
-const BUTTON_DISABLED_STYLE = "min-w-[268px] h-[56px] rounded-[28px] px-6 py-4 gap-[10px] bg-[#D9D9D9] text-white cursor-not-allowed";
+const BUTTON_DISABLED_STYLE = "min-w-[90px] h-[44px] rounded-[28px] px-6 py-4 gap-[10px] bg-[#D9D9D9] text-white cursor-not-allowed";
 
 export default function Button<T extends ElementType = "button">({
   as,
