f83209e3916b907d6b31b5da00a999f10f78202c
곽성재 <sungjae8970@gmail.com>
2025-05-09 23:03:53 +0900

    [FIX] Storybbok deploy fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
+const isStorybookExport = process.env.STORYBOOK === "true";
 
 const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/main',
+        source: "/",
+        destination: "/main",
         permanent: true, // 308 Permanent Redirect
       },
     ];
@@ -13,9 +14,12 @@ const nextConfig = {
   experimental: {
     turbo: false, // Turbopack 비활성화
   },
-  basePath: '',
-  assetPrefix: '',
-  output: 'standalone'
-}
+  basePath: "",
+  assetPrefix: isStorybookExport ? "/SWYP_FRONT/" : "",
+  images: {
+    unoptimized: isStorybookExport,
+  },
+  output: "standalone",
+};
 
 export default nextConfig;
