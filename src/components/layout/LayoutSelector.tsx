5dd102d327a7cea2338e878e489a015f1f29606b
곽성재 <sungjae8970@gmail.com>
2025-05-15 21:55:57 +0900

    [ADD] 전역 로딩상태 추가

--- src/components/layout/LayoutSelector.tsx
+++ src/components/layout/LayoutSelector.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import FullScreenLoader from '@/components/FullScreenLoader';
 import AppLayout from '@/components/layout/AppLayout';
 import MapLayout from '@/components/layout/MapLayout';
+import { useLoadingStore } from "@/store/useLoadingStore";
 import { usePathname } from 'next/navigation';
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 interface LayoutSelectorProps {
     children: ReactNode;
@@ -14,16 +16,16 @@ const DUAL_LAYOUT_PATHS = ['/travel/detail'];
 export default function LayoutSelector({ children }: LayoutSelectorProps) {
     const pathname = usePathname();
     const isMapPage = DUAL_LAYOUT_PATHS.some((path) => pathname.startsWith(path));
+    const isLoading = useLoadingStore((state) => state.isLoading);
 
-    console.log(isMapPage);
+    return (
+        <>
+            {isLoading ? <FullScreenLoader /> : null}
+            {isMapPage
+                ? <MapLayout>{children}</MapLayout>
+                : <AppLayout>{children}</AppLayout>
+            }
+        </>
+    )
 
-    return isMapPage ? (
-        <MapLayout>
-            {children}
-        </MapLayout>
-    ) : (
-        <AppLayout>
-            {children}
-        </AppLayout>
-    );
 }
