02b2cf83315523a899e89634b90b05a259cc9e42
Sungjae.Kwak <sungjae8970@gmail.com>
2025-05-17 13:52:33 +0900

    [FIX] 컨펌 모달 버튼 디자인 수정

--- src/components/modals/ConfirmModal.tsx
+++ src/components/modals/ConfirmModal.tsx
@@ -13,16 +13,17 @@ interface ConfirmModalProps {
   onClose?: () => void;
 }
 
-export default function ConfirmModal({
-  title,
-  description,
-  onCancel,
-  onConfirm,
-  children,
-  cancelText,
-  confirmText,
-  onClose
-}: ConfirmModalProps) {
+export default function
+  ConfirmModal({
+    title,
+    description,
+    onCancel,
+    onConfirm,
+    children,
+    cancelText,
+    confirmText,
+    onClose
+  }: ConfirmModalProps) {
   return (
     <BaseModal onClose={onClose ? onClose : onCancel}>
       <Text as='h2' textStyle='heading1' className='font-semibold text-center'>{title}</Text>
@@ -36,7 +37,7 @@ export default function ConfirmModal({
         <Button
           variant='cancel'
           textStyle='body1'
-          className="w-[50%] mr-2.5"
+          className="w-[50%] mr-2.5 hover:bg-[#EBE1FF] hover:text-[#9A77FF]"
           onClick={onCancel}
         >
           {cancelText}
@@ -44,7 +45,15 @@ export default function ConfirmModal({
         <Button
           variant='gradation'
           onClick={onConfirm}
-          className="w-[50%]"
+          className={`
+    w-[50%] text-white relative overflow-hidden
+    before:absolute before:inset-0 before:opacity-0 hover:before:opacity-100
+    before:transition-opacity
+    before:bg-[linear-gradient(125.9deg,_#9A77FF_23.39%,_#214BFF_104.52%)]
+    after:absolute after:inset-0 after:bg-black after:opacity-0 hover:after:opacity-20
+    after:transition-opacity
+    hover:text-white
+  `}
           textStyle='body1'
         >
           {confirmText}
